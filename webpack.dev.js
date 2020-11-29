7df247d153ffe25a58e200c424cc613744e7d842
Arkadiusz Kołodyński <arekkolodynski@poczta.fm>
2020-11-29 17:31:03 +0100

    deps update

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,4 @@
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const path = require("path");
 
@@ -6,19 +6,19 @@ module.exports = merge(common, {
   devtool: "#source-map",
   devServer: {
     contentBase: path.resolve(__dirname, "dist"),
-    port: process.env.npm_package_config_port
+    port: process.env.npm_package_config_port,
   },
   mode: "development",
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  }
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
 });
