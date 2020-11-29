a10c780b0bfb0fa6e9e82ed6b96db8bac3ef0949
Arkadiusz Kołodyński <arekkolodynski@poczta.fm>
2020-11-29 18:59:57 +0100

    ie fix

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,8 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
-const path = require("path");
 
 module.exports = merge(common, {
-  devtool: "#source-map",
-  devServer: {
-    contentBase: path.resolve(__dirname, "dist"),
-    port: process.env.npm_package_config_port,
-  },
+  devtool: "source-map",
   mode: "development",
   module: {
     rules: [
